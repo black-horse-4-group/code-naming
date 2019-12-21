757f809ed6cacf16a983c9355a06141ea566d59f
zhengzheng.qiao <zhengzheng.qiao@7fresh.com>
2019-12-21 12:13:39 +0800

    翻译接口

--- src/main/java/com/jd/intelligent/service/TestService.java
+++ src/main/java/com/jd/intelligent/service/TestService.java
@@ -4,17 +4,9 @@ package com.jd.intelligent.service;
 import com.jd.intelligent.api.NamingHandler;
 import com.jd.intelligent.beans.NamingRequest;
 import com.jd.intelligent.beans.Translation;
-import com.jd.intelligent.api.NamingHandler;
-import com.jd.intelligent.beans.NamingRequest;
-import com.jd.intelligent.beans.Translation;
 import com.jd.intelligent.beans.TranslationResult;
-import com.jd.intelligent.beans.TranslationResult;
-import com.jd.intelligent.common.util.DbUtil;
-import com.jd.intelligent.enums.TypeEnum;
 import com.jd.intelligent.enums.OptionEnum;
 import com.jd.intelligent.enums.TypeEnum;
-
-import java.sql.ResultSet;
 
 /**
  * Created by sunxuedong1 on 2019/12/13.
@@ -46,15 +38,15 @@ public class TestService {
 
 
 
-        NamingHandler namingHandler = new NamingHandler();
-        NamingRequest request = new NamingRequest();
-        request.setChineseWord("safdsad");
-        request.setOption(OptionEnum.QUERY);
-        request.setType(TypeEnum.INTERFACE);
-        TranslationResult result = namingHandler.translate(request);
-        for(Translation translation : result.getTranslations()){
-            System.out.println(translation.getWord());
-        }
+//        NamingHandler namingHandler = new NamingHandler();
+//        NamingRequest request = new NamingRequest();
+//        request.setChineseWord("safdsad");
+//        request.setOption(OptionEnum.QUERY);
+//        request.setType(TypeEnum.INTERFACE);
+//        TranslationResult result = namingHandler.translate(request);
+//        for(Translation translation : result.getTranslations()){
+//            System.out.println(translation.getWord());
+//        }
 
     }
 }
